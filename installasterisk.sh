110b47938d49b6c7ce137f874a48ad3788b90404
almeyras <69123882+almeyras@users.noreply.github.com>
2022-04-02 00:20:37 +0200

    Update installasterisk.sh

--- installasterisk.sh
+++ installasterisk.sh
@@ -14,9 +14,9 @@ apt install -y git-core subversion libjansson-dev sqlite autoconf automake libxm
 #el -y responde automaticamente yes a todas las preguntas
 
 cd /usr/src/
-sudo curl -O http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-18-current.tar.gz
-tar -zxvf asterisk-18-current.tar.gz
-rm  asterisk-18-current.tar.gz
+sudo curl -O http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-16-current.tar.gz
+tar -zxvf asterisk-16-current.tar.gz
+rm  asterisk-16-current.tar.gz
 cd asterisk*
 #como el nombre de la carpeta cambia con el número de versión, le pongo asterisco
 
